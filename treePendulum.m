6e804b07febdf2c153a0f04d59d01ff5a0ac1246
Matthew Beaudouin-Lafon <mlafon@olin.edu>
2015-11-30 11:17:22 -0500

    I can commit!

---
+++ treePendulum.m
@@ -0,0 +1,9 @@
+function res = treePendulum(theta1, theta2, theta3)
+
+g = 9.81;       % m.s^-2    Gravity constant
+m = 1;          % kg        Mass of thing
+l = 1;          % m         Length of rod
+
+
+
+end
---
+++ triplePendulum.m
@@ -0,0 +1,11 @@
+function res = triplePendulum(theta1, theta2, theta3)
+
+g = 9.81;       % m.s^-2    Gravity constant
+m = 1;          % kg        Mass of thing
+L = 1;          % m         Length of rod
+
+
+
+    function Res = updateAngles(Angles, 
+
+end
